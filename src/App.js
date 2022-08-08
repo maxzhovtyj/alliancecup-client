2cae8b00946bf154940f2749ce1b97149612c863
Zhovtaniuk Maxym <zhovtyjshady@gmail.com>
2022-08-08 18:32:45 +0300

    header initialized

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import './App.css';
 
+import HeaderComponent from './components/header/header.component'
+import {Route, Routes} from "react-router-dom";
+
 function App() {
   return (
     <div className="App">
-
+      <HeaderComponent/>
+        <Routes>
+            <Route path="/"/>
+        </Routes>
     </div>
   );
 }
---
+++ src/components/footer/footer.component.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function FooterComponent(props) {
+    return (
+        <footer>
+            Footer Component
+        </footer>
+    );
+}
+
+export default FooterComponent;
