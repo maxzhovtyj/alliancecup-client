fe04fe9b80e1fbd3ceba5b38ea935dbe6c6d290a
Zhovtaniuk Maxym <zhovtyjshady@gmail.com>
2022-08-10 14:09:02 +0300

    footer and media queries added

--- src/App.js
+++ src/App.js
@@ -1,17 +1,20 @@
 import './App.css';
 
 import HeaderComponent from './components/header/header.component'
+import FooterComponent from "./components/footer/footer.component";
+
 import {Route, Routes} from "react-router-dom";
 
 function App() {
-  return (
-    <div className="App">
-      <HeaderComponent/>
-        <Routes>
-            <Route path="/"/>
-        </Routes>
-    </div>
-  );
+    return (
+        <div className="App">
+            <HeaderComponent/>
+            <Routes>
+                <Route path="/"/>
+            </Routes>
+            <FooterComponent/>
+        </div>
+    );
 }
 
 export default App;
