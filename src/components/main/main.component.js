670f95f7e0effbf945e71c889b63585d68baa55b
Zhovtaniuk Maxym <zhovtyjshady@gmail.com>
2022-08-10 16:49:47 +0300

    navbar added

---
+++ src/components/main/main.component.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import NavbarComponent from "./navbar/navbar.component";
+
+function MainComponent(props) {
+    return (
+        <div>
+            <NavbarComponent/>
+            Main
+        </div>
+    );
+}
+
+export default MainComponent;
