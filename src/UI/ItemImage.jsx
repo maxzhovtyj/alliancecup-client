27a617766de98ab1fef2d4627339ecf175a1881f
Zhovtaniuk Maxym <zhovtyjshady@gmail.com>
2023-07-22 21:06:00 +0300

    add image waving

--- src/UI/ItemImage.jsx
+++ src/UI/ItemImage.jsx
@@ -2,14 +2,16 @@ import configData from "../config.json";
 
 import NoImageItem from "../assets/NoImageItem.png"
 
-const ItemImage = ({cls,  plug = true, alt = "item-image", item: {imgUrl, imgUUID}}) => {
+import classes from "./ItemImage.module.scss"
+
+const ItemImage = ({cls,  plug, alt = "item-image", item: {imgUrl, imgUUID}}) => {
     if (imgUUID) {
         const imgUrlUUID = `${configData.MINIO_URL}/images/${imgUUID}`
-        return <img loading={"lazy"} src={imgUrlUUID} alt={alt} className={cls}/>
+        return <img loading={"lazy"} src={imgUrlUUID} alt={alt} className={`${cls} ${classes.skeletonImage}`}/>
     } else if (imgUrl) {
-        return <img loading={"lazy"} src={imgUrl} alt={alt} className={cls}/>
+        return <img loading={"lazy"} src={imgUrl} alt={alt} className={`${cls} ${classes.skeletonImage}`}/>
     } else if (plug) {
-        return <img loading={"lazy"} src={NoImageItem} alt={"empty-img"} className={cls}/>
+        return <img loading={"lazy"} src={NoImageItem} alt={"empty-img"} className={`${cls} ${classes.skeletonImage}`}/>
     }
 
     return <div className={cls}/>
